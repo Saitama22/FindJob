2dbd93c0eb5d3d7f056d0d7e7f56371f0a81172f
Saitama22 <ezikovs99@gmail.com>
2022-06-13 17:36:28 +0300

    first commit

---
+++ DI.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindJob
+{
+    public static class DI
+    {
+        public static IServiceCollection Init(this IServiceCollection services)
+        {
+            services.AddMvc();
+            return services;
+        }
+    }
+}
