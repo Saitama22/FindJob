d9fdfd2249a5d884c4b0d04d4678063c9660cc13
Saitama22 <ezikovs99@gmail.com>
2022-06-13 18:21:53 +0300

    конфигурирование приложения

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace FindJob
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Init();
+            services.Init();                        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,7 +28,12 @@ namespace FindJob
             }
 
             app.UseRouting();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            //app.UseMvc(routes =>
+            //{
 
+            //});
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
